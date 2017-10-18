79a465499d91e5469ad1e26b8361125743b4dbdd
KokoTa <584847514@qq.com>
2017-10-18 14:47:28 +0800

    fix main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,40 +1,37 @@
-let wrap = document.querySelector('.wrap');
-let select = document.querySelector('.select');
-let marks = Array.from(wrap.querySelectorAll('p'));
-let impression_num = document.querySelector('#impression_num');
+'use strict';
+
+var wrap = document.querySelector('.wrap');
+var select = document.querySelector('.select');
+var marks = Array.prototype.slice.call(wrap.querySelectorAll('p'));
+var impression_num = document.querySelector('#impression_num');
 impression_num.textContent = marks.length;
 wrap.innerHTML = '';
 
-let ul = document.createElement('ul');
-let start = 0;
-let length = 20;
-let num = Math.ceil(marks.length / 20);
-let selectData = marks.slice(start, length);
+var ul = document.createElement('ul');
+var start = 0;
+var length = 20;
+var num = Math.ceil(marks.length / 20);
+var selectData = marks.slice(start, length);
 
-console.log(marks.length)
-
-let draw = function(data) {
-	let html = '';
-	data.forEach((item, i) => {
+var draw = function draw(data) {
+	var html = '';
+	data.forEach(function (item, i) {
 		html += item.outerHTML;
 	});
 	wrap.innerHTML = html;
-}
+};
 
-for(let i = 1; i < num+1; i++) {
-	let option = document.createElement('option');
+for (var i = 1; i < num + 1; i++) {
+	var option = document.createElement('option');
 	option.setAttribute('value', i);
 	option.textContent = '第' + i + '页';
 	select.appendChild(option);
 }
 
-select.addEventListener('change', (e) => {
-	let start = e.target.value - 1;
-	selectData = marks.slice(start*length, (start+1)*length);
+select.addEventListener('change', function (e) {
+	var start = e.target.value - 1;
+	selectData = marks.slice(start * length, (start + 1) * length);
 	draw(selectData);
 });
 
 draw(selectData);
-
-
-
