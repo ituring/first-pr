5106f02b2e1c3ccbd2e672e0adec6fdd3ff7e45b
michaelwng <michaelwangg@qq.com>
2019-07-14 17:24:20 +0800

    增加一个leet code的代码，两数之和

---
+++ src/Two_Sum.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+    def twoSum(self, arr, target):
+		#用一个字典记录每个元素的值，以及下标	
+		d = {}
+		for i,v in enumerate(arr):
+			d[v] = i
+		
+		for i,v in enumerate(arr):
+			x = target - v
+			#如果target-v的值在字典中，并且这个值的下标和i不同，就是要求的值
+			if(d.has_key(x) and i!=d[x]):
+				return [d[x],i]
+		return []	
