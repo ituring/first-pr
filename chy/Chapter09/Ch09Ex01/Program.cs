f6428ad08da9939ae9eed2e3028798692f8a78fd
lingyuqi123 <2019185519@qq.com>
2020-09-03 09:04:05 +0800

    da

---
+++ chy/Chapter09/Ch09Ex01/Program.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Ch09Ex01
+{
+   public abstract class MyBase {}
+
+   internal class MyClass : MyBase {}
+
+   public interface IMyBaseInterface {}
+
+   internal interface IMyBaseInterface2 {}
+
+   internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2 {}
+
+   internal sealed class MyComplexClass : MyClass, IMyInterface {}
+
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         MyComplexClass myObj = new MyComplexClass();
+         WriteLine(myObj.ToString());
+         ReadKey();
+            Class1 class1 = new Class1();
+            class1.
+         
+      }
+   }
+}
---
+++ chy/Chapter09/Ch09Ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch09ClassLib;
+
+using static System.Console;
+
+namespace Ch09Ex02
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         MyExternalClass myObj = new MyExternalClass();
+         WriteLine(myObj.ToString());
+         ReadKey();
+
+      }
+   }
+}
